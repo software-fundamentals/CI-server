31fdbe22a2eee4fb52601db41fa6052586fa16e7
miguel <miguelmu@kth.se>
2019-01-31 14:30:56 +0100

    Fix #3 Set up Gradle

---
+++ src/test/java/ciserver/ContinuousIntegrationServerTest.java
@@ -0,0 +1,12 @@
+package ciserver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ContinuousIntegrationServerTest {
+	@Test
+	public void testSample() {
+		fail();
+	}
+}
