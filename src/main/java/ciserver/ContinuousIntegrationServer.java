01243ea22fea75bd42885a6af9f08d015c3a525f
williamwesterlund <william.e.westerlund@gmail.com>
2019-01-31 16:09:12 +0100

    can read request body

--- src/main/java/ciserver/ContinuousIntegrationServer.java
+++ src/main/java/ciserver/ContinuousIntegrationServer.java
@@ -4,7 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
@@ -27,13 +30,49 @@ import org.eclipse.jetty.server.handler.AbstractHandler;
         baseRequest.setHandled(true);
 
         System.out.println(target);
+        System.out.println("Request");
+        System.out.println(request);
+        System.out.println(baseRequest.getContext());
+        System.out.println("Response");
+        System.out.println(response);
+
+
+        String body = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        System.out.println(body);
+        response.getWriter().println(body);
+        //response.getWriter().println("CI job done");
+//        Runtime rt = Runtime.getRuntime();
+//        Process pr = rt.exec("gradle build");
+//        Process ps = rt.exec("gradle test");
+
+        final Process p = Runtime.getRuntime().exec("gradle build");
+
+        new Thread(new Runnable() {
+            public void run() {
+                BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                String line = null;
+
+                try {
+                    while ((line = input.readLine()) != null)
+                        System.out.println(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        try {
+            p.waitFor();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         // here you do all the continuous integration tasks
         // for example
         // 1st clone your repository
         // 2nd compile the code
 
-        response.getWriter().println("CI job done");
+
     }
 
     // used to start the CI server in command line
