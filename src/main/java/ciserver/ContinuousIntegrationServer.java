b80ea2777d99bce66562b213f28205d8c4322155
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-02-05 21:06:52 +0100

    Work on #12 Continue working on cloning a git repository using JGIT

--- src/main/java/ciserver/ContinuousIntegrationServer.java
+++ src/main/java/ciserver/ContinuousIntegrationServer.java
@@ -17,6 +17,14 @@ import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.json.*;
 import org.json.JSONArray;
 
+import java.util.Arrays;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
 /**
   Skeleton of a ContinuousIntegrationServer which acts as webhook
   See the Jetty documentation for API documentation of those classes.
@@ -38,8 +46,19 @@ public class ContinuousIntegrationServer extends AbstractHandler
         System.out.println(request.getMethod());
 
         JSONObject obj = new JSONObject(request.getReader().lines().collect(Collectors.joining(System.lineSeparator())));
-        String ssh_url = obj.getJSONObject("repository").getString("ssh_url");
-        String branch = obj.getString("ref").split("refs/heads/")[1];
+        String url = obj.getJSONObject("repository").getString("url");
+        String branch = obj.getString("ref");
+
+        try {
+            Git repo = Git.cloneRepository()
+            .setURI(url)
+            .setDirectory(new File("~/CI"))
+            .call();
+            System.out.println(repo.getRepository());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
 //        EXAMPLE: This is how to get strings and object from the payload
 //        System.out.println(obj.getJSONObject("repository").getString("ssh_url"));
