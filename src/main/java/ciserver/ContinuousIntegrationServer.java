1a5f15be7c7eedcaae6c717ee59a87129858a5b5
miguel <miguelmu@kth.se>
2019-02-06 15:53:49 +0100

    Fix gradle errors

--- src/main/java/ciserver/ContinuousIntegrationServer.java
+++ src/main/java/ciserver/ContinuousIntegrationServer.java
@@ -1,13 +1,13 @@
 package ciserver;
 
-import javax.servlet.ServletInputStream;
+//import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
-import java.io.BufferedReader;
+//import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+//import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
 import org.eclipse.jetty.server.Server;
@@ -15,15 +15,15 @@ import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
 import org.json.*;
-import org.json.JSONArray;
+//import org.json.JSONArray;
 
-import java.util.Arrays;
+//import java.util.Arrays;
 
 import java.io.File;
 import java.io.IOException;
 
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
+//import org.eclipse.jgit.api.errors.GitAPIException;
 
 /**
   Skeleton of a ContinuousIntegrationServer which acts as webhook
@@ -51,17 +51,17 @@ public class ContinuousIntegrationServer extends AbstractHandler
 
         try {
             Git repo = Git.cloneRepository()
-            .setURI(url)
-            .setDirectory(new File("~/CI"))
-            .call();
+                .setURI(url)
+                .setDirectory(new File("~/CI"))
+                .call();
             System.out.println(repo.getRepository());
         } catch (Exception e) {
             e.printStackTrace();
         }
 
 
-//        EXAMPLE: This is how to get strings and object from the payload
-//        System.out.println(obj.getJSONObject("repository").getString("ssh_url"));
+        //        EXAMPLE: This is how to get strings and object from the payload
+        //        System.out.println(obj.getJSONObject("repository").getString("ssh_url"));
 
         response.getWriter().println("CI up and running");
 
@@ -71,28 +71,28 @@ public class ContinuousIntegrationServer extends AbstractHandler
         // 2nd compile the code
 
 
-//        TODO: Make the gradle build code below a complete and standalone function
-//        final Process p = Runtime.getRuntime().exec("gradle build");
-//
-//        new Thread(new Runnable() {
-//            public void run() {
-//                BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-//                String line = null;
-//
-//                try {
-//                    while ((line = input.readLine()) != null)
-//                        System.out.println(line);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }).start();
-//
-//        try {
-//            p.waitFor();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        //        TODO: Make the gradle build code below a complete and standalone function
+        //        final Process p = Runtime.getRuntime().exec("gradle build");
+        //
+        //        new Thread(new Runnable() {
+        //            public void run() {
+        //                BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        //                String line = null;
+        //
+        //                try {
+        //                    while ((line = input.readLine()) != null)
+        //                        System.out.println(line);
+        //                } catch (IOException e) {
+        //                    e.printStackTrace();
+        //                }
+        //            }
+        //        }).start();
+        //
+        //        try {
+        //            p.waitFor();
+        //        } catch (InterruptedException e) {
+        //            e.printStackTrace();
+        //        }
 
 
     }
