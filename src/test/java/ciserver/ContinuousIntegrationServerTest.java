b7dc4395f9d04d7c7c1436adebc148f6f2582d2b
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-02-07 15:42:04 +0100

    Add tests for function parseJSON

--- src/test/java/ciserver/ContinuousIntegrationServerTest.java
+++ src/test/java/ciserver/ContinuousIntegrationServerTest.java
@@ -4,9 +4,49 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import org.json.*;
+
 class ContinuousIntegrationServerTest {
     @Test
     public void testSample() {
         //fail();
+    }
+
+    /**
+     * Tests the functionality of the parseJSON function, both that
+     * the error handling works and that it returns the url and ref
+     * of requests on the correct format,
+     */
+    @Test
+    public void testJSONErrorHandling() {
+        ContinuousIntegrationServer ci = new ContinuousIntegrationServer();
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        try { //An empty request
+            ci.parseJSON(request);
+            fail("An empty request should result in an error");
+        } catch (java.lang.Error e) {}
+
+        byte[] b = "dummy".getBytes();
+        request.setContent(b);
+        try { //A request with wrong content
+            ci.parseJSON(request);
+            fail("A request with wrong content should also result in an error");
+        } catch (java.lang.Error e) {}
+
+
+        MockHttpServletRequest request2 = new MockHttpServletRequest();
+        JSONObject obj = new JSONObject();
+        JSONObject repo = new JSONObject();
+        repo.put("url", "test_url");
+        obj.put("repository", repo);
+        obj.put("ref", "test_ref");
+        byte[] jsonBytes = obj.toString().getBytes();
+        request2.setContent(jsonBytes); //A request on the correct format
+        String[] result = ci.parseJSON(request2);
+
+        assertEquals(result[0], "test_url");
+        assertEquals(result[1], "test_ref");
     }
 }
