d9cb73ce48c5086493dcdfcf97c3869f91aa8165
MiggL <miguelmu@kth.se>
2019-02-07 16:58:46 +0100

    Fix #20 (#34)
    clean up code

--- src/main/java/ciserver/ContinuousIntegrationServer.java
+++ src/main/java/ciserver/ContinuousIntegrationServer.java
@@ -1,6 +1,5 @@
 package ciserver;
 
-//import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
@@ -13,27 +12,26 @@ import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
 import org.json.*;
-//import org.json.JSONArray;
-
-//import java.util.Arrays;
 
 import java.io.IOException;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
-//import org.eclipse.jgit.transport.RefSpec;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.JGitInternalException;
 
 /**
-  Skeleton of a ContinuousIntegrationServer which acts as webhook
-  See the Jetty documentation for API documentation of those classes.
-  */
+ * Skeleton of a ContinuousIntegrationServer which acts as webhook
+ * See the Jetty documentation for API documentation of those classes.
+ */
 public class ContinuousIntegrationServer extends AbstractHandler
 {
     public ContinuousIntegrationServer() {}
 
+    /**
+     * @Return {url, ref}
+     */
     public String[] parseJSON(HttpServletRequest request) {
         String[] parsedData = new String[2];
         try {
@@ -62,14 +60,16 @@ public class ContinuousIntegrationServer extends AbstractHandler
             Request baseRequest,
             HttpServletRequest request,
             HttpServletResponse response)
-            throws IOException, ServletException {
+            throws ServletException {
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
 
-
-        System.out.println("Request method:");
-        System.out.println(request.getMethod());
+        try {
+            response.getWriter().println("CI up and running");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         String[] parsedData = parseJSON(request);
         if (parsedData == null)
@@ -78,6 +78,19 @@ public class ContinuousIntegrationServer extends AbstractHandler
         String branch = parsedData[1];
 
         String cloneDir = "~/CI";
+        cloneRepo(url, cloneDir);
+        pullBranch(cloneDir, branch);
+
+        try { 
+            GradleBuildOutput output = runGradle(cloneDir);
+            System.out.println(output.result);
+            System.out.println(output.log);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void cloneRepo(String url, String cloneDir) {
         try {
             Git repo = Git.cloneRepository()
                 .setURI(url)
@@ -86,13 +99,17 @@ public class ContinuousIntegrationServer extends AbstractHandler
             System.out.println(repo.getRepository());
             System.out.println("Successfully cloned");
         } catch (JGitInternalException e) {
-            System.out.println("Destination path already exists and is not an empty directory");
+            if (e.getMessage().endsWith(" already exists and is not an empty directory"))
+                System.out.println("Repo already exists, clone not needed.");
         } catch (GitAPIException e) {
             e.printStackTrace();
         }
+    }
+
+    private void pullBranch(String gitDir, String branch) {
         try {
             Repository fr = new FileRepositoryBuilder()
-                .setGitDir(new File(cloneDir + "/.git"))
+                .setGitDir(new File(gitDir + "/.git"))
                 .readEnvironment()
                 .findGitDir()
                 .build();
@@ -100,30 +117,14 @@ public class ContinuousIntegrationServer extends AbstractHandler
             git.checkout().setName("master").call();
             git.pull().call();
             System.out.println("Successfully pulled from origin");
-            //RefSpec rs = new RefSpec(String.format("%1$s:%1$s", branch));
-            //git.fetch().setRefSpecs(rs).call();
-            //System.out.println("Successfully fetched branch " + branch);
             String shortBranchName = branch.split("refs/heads/")[1];
             git.checkout().setName("origin/" + shortBranchName).call();
             System.out.println("Successfully checked out branch " + shortBranchName);
         } catch (GitAPIException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        //        EXAMPLE: This is how to get strings and object from the payload
-        //        System.out.println(obj.getJSONObject("repository").getString("ssh_url"));
-
-        response.getWriter().println("CI up and running");
-
-        // here you do all the continuous integration tasks
-        // for example
-        // 1st clone your repository
-        // 2nd compile the code
-
-        GradleBuildOutput output = runGradle(cloneDir);
-        System.out.println(output.result);
-        System.out.println(output.log);
-
     }
 
     private GradleBuildOutput runGradle(String dir) throws IOException {
@@ -133,7 +134,6 @@ public class ContinuousIntegrationServer extends AbstractHandler
             public void run() {
                 String line = null;
                 BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
                 try {
                     while ((line = input.readLine()) != null) {
                         successData.append(line + "\n");
@@ -143,7 +143,6 @@ public class ContinuousIntegrationServer extends AbstractHandler
                 }
             }
         }).start();
-
         try {
             p.waitFor();
             if (p.exitValue() != 0) {
@@ -170,7 +169,7 @@ public class ContinuousIntegrationServer extends AbstractHandler
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception
     {
-        Server server = new Server(8989);
+        Server server = new Server(8001);
         server.setHandler(new ContinuousIntegrationServer());
         server.start();
         server.join();
