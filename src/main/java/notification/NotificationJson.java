563eb55a194125e7f6fa1ffc54ed97a66d5ee633
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-02-08 09:01:01 +0100

    Fix #37 Change fallback status

--- src/main/java/notification/NotificationJson.java
+++ src/main/java/notification/NotificationJson.java
@@ -7,7 +7,7 @@ public class NotificationJson {
 
     public static JSONObject createSlackJson(String authorName, String authorUrl, String title, String titleLink, String text, String color) {
         JSONObject attachment = new JSONObject();
-        attachment.put("fallback", "Unable to display repository status");
+        attachment.put("fallback", "New build: " + title);
         attachment.put("author_name", authorName);
         attachment.put("author_url", authorUrl);
         attachment.put("title", title);
